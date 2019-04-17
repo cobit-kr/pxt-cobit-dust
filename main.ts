c9a19e0a99f3e1bfaad2a17bfe7930e53e199966
conner98 <conner.jeong@gmail.com>
2019-04-17 23:32:59 +0900

    edit dust block

--- main.ts
+++ main.ts
@@ -1,7 +1,23 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+/**
+ * Functions to operate the cobit dust sensor
+ */
+//% color=#f44242 icon="\uf1b9" block="cobit-dust"
+namespace cobit_dust {
+
+    /**
+	 *  Read dust sensor
+	 */
+    //% weight=90
+    //% blockId="cobit-dust_readDustsensor" block="미세먼지센서 읽기"
+    export function readDustsensor(): number {
+        let value = 0
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        basic.pause(280)
+        value = pins.analogReadPin(AnalogPin.P1)
+        basic.pause(40)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        basic.pause(9680)
+        value = value * (5 / 1024)
+        return value
+    }
+}
